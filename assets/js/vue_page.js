3df329d4a540955eb5860d998a26775b515cc57a
Cubik65536 <cubik65536@cubik65536.top>
2022-06-16 13:53:08 -0400

    Chinese version of website

--- assets/js/vue_page.js
+++ assets/js/vue_page.js
@@ -9,6 +9,15 @@ var footerHTML =
     '   HTML theme designed by <a href="https://github.com/athul/archie" rel="nofollow noopener noreferrer">Archie Theme</a>, modified by <a href="https://github.com/KevinZonda" rel="nofollow noopener noreferrer">@KevinZonda</a>.' +
     '</footer>';
 
+var zhFooterHTML =
+    '<footer>' +
+    '   <hr>' +
+    '   <a href="https://github.com/iXORTech" title="GitHub"><i data-feather="github"></i></a>' +
+    '   | Copyright &copy; 2022 iXOR Technology. 保留所有权利' +
+    '   <br/>' +
+    '   HTML 主题由 <a href="https://github.com/athul/archie" rel="nofollow noopener noreferrer">Archie Theme</a> 设计，并由 <a href="https://github.com/KevinZonda" rel="nofollow noopener noreferrer">@KevinZonda</a> 制作' +
+    '</footer>';
+
 function buildOnce(genApp, hookDom) {
   var vueInstance = Vue.createApp(
     Object.assign(
@@ -28,6 +37,7 @@ buildOnce(function () {
     data() {
       return {
         footer: footerHTML,
+        zhFooter: zhFooterHTML
       };
     },
   };
